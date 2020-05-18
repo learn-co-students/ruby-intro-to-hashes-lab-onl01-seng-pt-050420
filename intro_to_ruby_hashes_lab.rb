e3f9439fa758c8617035b17c344d7eaafac1f092
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-18 02:34:36 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 require 'pry'
-  
+
 def new_hash
- test_hash = hash.new 
+ new_hash = {} 
 end
 
 def actor 
@@ -11,41 +11,14 @@ end
 def monopoly
   monopoly = {}
   monopoly[:railroads] = {}
+  monopoly 
 end 
-  
-monopoly
 
+monopoly 
 
-# The following method won't work... the correct way is below:
-
-# def monopoly_with_third_tier
-#  monopoly[:pieces] = 4,
-#  monopoly[:rent_in_dollars] = "one_piece_owned", "two_pieces_owned", "three_pieces_owned", "four_pieces_owned"
-#  monopoly[:names] = "reading_railroad", "pennsylvania_railroad", "b_and_o_railroad", "shortline"
-# end 
-
-
-# This one did not pass the learn.co test because of the following error message:
-
-# expected: [:pieces, :names, :rent_in_dollars]
-# got:      [:pieces, :rent_in_dollars, :names]
-
-
-# def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-#   monopoly = {
-#     railroads: {
-#       pieces: 4,
-#       rent_in_dollars: {}, 
-#       names: {}
-#     }
-#   }
-# end
-
-# This one passed correctly:
 
 def monopoly_with_second_tier
-#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+
   monopoly = {
     railroads: {
      pieces: 4,
@@ -56,30 +29,6 @@ def monopoly_with_second_tier
 end
 
 monopoly_with_second_tier
-
-
-# This method is also incorrect because it adds a hash key as a string
-# "mortgage_value" is a hash key, so it can't be entered like this.
-# The correct way is shown in the next example below:
-
-#def monopoly_with_third_tier
-#  monopoly = {
-#    railroads: {
-#      pieces: 4,                               <===apparently you can enter an integer like this...
-#      rent_in_dollars: { 
-#        :one_piece_owned => 25,                  <=== ...or like this
-#        :two_pieces_owned => 50,                 <=== ,this
-#        :three_pieces_owned => 100,              <=== ,this
-#        :four_pieces_owned => 200                <=== ,and this   
-#      }, 
-#      names: {
-#        reading_railroad: "mortgage_value",      <===here's the problem
-#        pennsylvania_railroad: "mortgage_value", <===same here
-#        b_and_o_railroad: "shortline"            <===and here
-#      }
-#    }
-#  }
-#end 
 
 def monopoly_with_third_tier
   monopoly = {
